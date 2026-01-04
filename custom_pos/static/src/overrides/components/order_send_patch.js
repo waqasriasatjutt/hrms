2eabf5131037e82355cc6a174d252d61d31cea9d
waqas riasat <waqasriasatjutt@gmail.com>
2026-01-04 16:50:00 +0500

    push updates pos qz central server pos fix version issue

--- custom_pos/static/src/overrides/components/order_send_patch.js
+++ custom_pos/static/src/overrides/components/order_send_patch.js
@@ -7,12 +7,14 @@ import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/
 patch(PosStore.prototype, {
     async sendOrderInPreparationUpdateLastChange(order, cancelled = false) {
         // 🔹 Call original Odoo method (IMPORTANT)
-        await this._super(order, cancelled);
+        // await this._super(order, cancelled);
+        const result = super.sendOrderInPreparationUpdateLastChange(order, cancelled = false);
 
         // 🔹 Your custom logic only
         if (order && !cancelled) {
             order.markAsSentToKitchen();
             console.log("✅ Marked as sent to kitchen:", order.uid);
         }
+        return result;
     },
 });
